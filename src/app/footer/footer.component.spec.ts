a5b5ebf8718ecaa921f2f5805c7c395890c8d27b
Chintha Srujana <157452984+SrujanaChintha@users.noreply.github.com>
2024-08-30 10:42:10 +0530

    deleted

--- src/app/footer/footer.component.spec.ts
+++ src/app/footer/footer.component.spec.ts
@@ -5,10 +5,7 @@
 
   
 
-    fixture = TestBed.createComponent(FooterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+   
 
   it('should create', () => {
     expect(component).toBeTruthy();
